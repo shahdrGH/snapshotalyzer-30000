7753861face0c01ea79f16d74e3fd54d3806ab6d
shahdrGH <63173571+shahdrGH@users.noreply.github.com>
2020-10-14 09:28:40 -0500

    List Volumes and Snapshots. Naive implementation of snapshot command

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -80,6 +80,21 @@ def list_volumes(project):
 def instances():
     """Commands for instances"""
 
+@instances.command('snapshot', help="Create snapshots of all volumes")
+@click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+    "Create snapshots for EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print("Creating snapshot of  {0}.....".format(v.id))
+            v.create_snapshots(Description="Created by SnapshotAlyzer 30000")
+
+    return
+
+
 @instances.command('list')
 @click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
 def list_instances(project):
